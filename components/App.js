6ef8cc6f7c2fe8ad0bd8db79362c23598b735294
Swapnil <swp.spiky@gmail.com>
2016-06-04 21:11:01 -0600

    Get ready with react

--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, {Component} from 'react' //So that we dont have to use React.component always.
+
+class App extends Component {
+    render() {
+        return (
+          <div>This is react component11...!!</div>
+        );
+    }
+}
+export default App
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,23 @@
-
+//Write your own webpack configuration.
+module.exports = {
+    entry:['./client/client.js'],
+    output : {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader', //babel is used to compile jsx or ES6
+                exclude: /node_modules/,
+                //query will tell babel what to do with all java script file
+                //Compile Jsx & ES6 to java script
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
